41fcdecea12715a9182b78892b5a1e1907c0d455
EmmaWalker6 <emma.walker2410@gmail.com>
2025-02-03 23:28:51 +1100

    most of design done just need to fix search button

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import './Weather.css'
 import searchIcon from '../assets/searchIcon.png'
+import cloudyImage from '../assets/cloudy.png'
+import drizzleImage from '../assets/drizzle.png'
+import partlyCloudyImage from '../assets/partlyCloudy.png'
+import snowingImage from '../assets/snowing.png'
+import sunnyImage from '../assets/sunny.png'
+
+// Images from cleanpng
+
 
 const Weather = () => {
   return (
@@ -10,7 +18,13 @@ const Weather = () => {
                 <input type ="text" placeholder='Search'></input>
                 <img id='searchButton' src={searchIcon} alt=''/>
             </div>
+            <div className='weatherContent'>
+                <img src={sunnyImage} alt="" className='weatherIcon'/>
+                <p className='temperature'>16-c</p>
+                <p className='location'>London</p>
+            </div>
         </div>
+        {/* possibly add in huminity and wind speed */}
     </div>
   )
 }
