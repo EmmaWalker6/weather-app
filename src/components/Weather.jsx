8d9ac0c9db79ce516c111e1d0e5115e8e5a42949
EmmaWalker6 <emma.walker2410@gmail.com>
2025-02-03 22:49:07 +1100

    adding in images

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -7,6 +7,7 @@ const Weather = () => {
         <div className='weather'>
             <div className='searchBar'>
                 <input type ="text" placeholder='Search'></input>
+                <button>Search</button>
                 {/* <img src="" alt/> */}
             </div>
         </div>
