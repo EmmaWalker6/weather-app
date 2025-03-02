6aae5333fa67fa00b58d8a7659f4122759573e24
EmmaWalker6 <emma.walker2410@gmail.com>
2025-03-02 15:40:23 +1100

    we now have location and can switch from f to c. but we need to improve the switch

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Weather.css'
 import searchIcon from '../assets/searchIcon.png'
 import cloudyImage from '../assets/cloudy.png'
@@ -12,7 +12,11 @@ import sunnyImage from '../assets/sunny.png'
 
 const Weather = () => {
 
+  const inputRef = useRef();
+
   const [weatherData, setWeatherData] = useState(false);
+  const [isMetric, setIsMetric] = useState(true);
+  const [unitType, setUnitType] = useState({ stringValue: "metric" });
 
   const allIcons = {
     // Icon ids and what image we attach to them. The codes go for day and night and the description is commented on the side of each day
@@ -20,8 +24,8 @@ const Weather = () => {
     "01n": sunnyImage,
     "02d": partlyCloudyImage, //few clouds
     "02n": partlyCloudyImage,
-    "03d": cloudyImage, //scattered clouds
-    "03n": cloudyImage,
+    "03d": partlyCloudyImage, //scattered clouds
+    "03n": partlyCloudyImage,
     "04d": cloudyImage, //broken clouds
     "04n": cloudyImage,
     "09d": drizzleImage, //rain shower
@@ -38,13 +42,28 @@ const Weather = () => {
 
   const search = async (city) => {
     try {
+      if(city === ""){
+        alert("Enter City Name");
+        return;
+      }
       const apiKey = process.env.REACT_APP_ID;
       console.log("Using API Keyy:", apiKey);
 
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+      if(isMetric){
+        setUnitType({ stringValue: "metric" });
+      }else{
+        setUnitType({ stringValue: "imperial" });
+      }
+
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unitType.stringValue}&appid=${apiKey}`;
 
       const response = await fetch(url);
       const data = await response.json();
+
+      if(!response.ok){
+        alert(data.message);
+        return;
+      }
       console.log(data);
 
       const icon = allIcons[data.weather[0].icon] || sunnyImage;
@@ -58,26 +77,42 @@ const Weather = () => {
 
     } catch (error) {
       console.error("Error fetching weather data:", error);
+      setWeatherData(false);
     }
   }
 
-  useEffect(()=>{
-    search("Cameron Park")
-  }, [])
+  const switchDegreeType = () => {
+    setIsMetric(!isMetric);
+    if(inputRef.current.value !== ''){
+      search(inputRef.current.value);
+    }
+    
+  }
+
+  // useEffect(()=>{
+  //   search("Sydney")
+  // }, [])
 
   return (
     <div>
         <div className='weather'>
             <div className='searchBar'>
-                <input type ="text" placeholder='Search'></input>
-                <img id='searchButton' src={searchIcon} alt=''/>
+                <p id='switchFtC' onClick={switchDegreeType}>{isMetric ? 'C': 'F'}</p>
+                <input ref={inputRef} type ="text" placeholder='Search'></input>
+                <img id='searchButton' src={searchIcon} alt='' onClick={()=>search(inputRef.current.value)}/>
+                
             </div>
-            <div className='weatherContent'>
+            {/* Checking if we actually have weather data to display */}
+            {weatherData?<>
+              <div className='weatherContent'>
                 <img src={weatherData.icon} alt="" className='weatherIcon'/>
-                <p className='temperature'>{weatherData.temperature} °c</p>
+                <p className='temperature'>{weatherData.temperature} °{isMetric ? 'C': 'F'}</p>
                 <p className='location'>{weatherData.location}</p>
                 <p className='humidity'>Humidity: {weatherData.humidity}%</p>
             </div>
+            </>:<></>}
+            {/* Otherwise we have no data */}
+            
         </div>
         {/* possibly add in huminity and wind speed */}
     </div>
